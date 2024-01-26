4bbcaece5728c241489b728945f564beace4bc28
mr-possible <sambhavd4@gmail.com>
2024-01-26 22:29:19 +0000

    Added url converter

---
+++ src/main/java/com/samagra/config/converters/StringToURLConverter.java
@@ -0,0 +1,15 @@
+package com.samagra.config.converters;
+
+import lombok.SneakyThrows;
+import org.aeonbits.owner.Converter;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+
+public class StringToURLConverter implements Converter<URL> {
+    @SneakyThrows
+    @Override
+    public URL convert(Method method, String s) {
+        return new URL(s);
+    }
+}
