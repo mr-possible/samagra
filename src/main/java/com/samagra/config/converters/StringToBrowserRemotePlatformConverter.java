8988d5bd6cc0cf3be61dd468f4772ba932d6ce72
mr-possible <sambhavd4@gmail.com>
2024-02-01 22:59:41 +0530

    Fix browser remote platform code

--- src/main/java/com/samagra/config/converters/StringToBrowserRemotePlatformConverter.java
+++ src/main/java/com/samagra/config/converters/StringToBrowserRemotePlatformConverter.java
@@ -4,20 +4,10 @@ import com.samagra.enums.BrowserRemotePlatform;
 import org.aeonbits.owner.Converter;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 
 public class StringToBrowserRemotePlatformConverter implements Converter<BrowserRemotePlatform> {
     @Override
     public BrowserRemotePlatform convert(Method method, String browserRemotePlatform) {
-        Map<String, BrowserRemotePlatform> values = Map.of(
-                "selenium-grid", BrowserRemotePlatform.SELENIUM_GRID,
-                "browserstack", BrowserRemotePlatform.BROWSERSTACK
-        );
-
-        /**
-         * If the user mistakenly passes any un-supported platform,
-         * then run tests on selenium-grid as default (to ensure not to halt the process)
-         */
-        return values.getOrDefault(browserRemotePlatform, BrowserRemotePlatform.SELENIUM_GRID);
+        return BrowserRemotePlatform.valueOf(browserRemotePlatform.toUpperCase());
     }
 }
