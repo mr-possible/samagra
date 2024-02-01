4da9dd546bc45dbf887776194aa0dfeb2f68ca8d
mr-possible <sambhavd4@gmail.com>
2024-02-01 23:00:25 +0530

    Added sample base class for tests to extend

--- src/test/java/com/samagra/web/Base.java
+++ src/test/java/com/samagra/web/Base.java
@@ -1,10 +1,16 @@
 package com.samagra.web;
 
+import com.samagra.driver.Driver;
 import org.testng.annotations.BeforeTest;
 
 public class Base {
     @BeforeTest
     public void setup() {
+        Driver.initialiseDriver();
+    }
 
+    @BeforeTest
+    public void trash() {
+        Driver.quitDriver();
     }
 }
